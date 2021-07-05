e50beffabb02abb9cc97a8e41bdc4eb49bbd6a72
GIL911 <wjrlvh@gmail.com>
2021-07-05 22:31:19 +0900

    2021.07.05 commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    #write
+    path('write/', views.write, name='write'),
+    #List
+    path('List/', views.list, name='list'),
+    # view
+    #re_path(r'^post(.*)/$', blog_views.post),
+    #re_path(r'^view/([0-9])/$', views.view, name='view')
+    re_path(r'^view/(?P<num>[0-9]+)/$', views.view, name='view')
 ]
